5cd77ad61e8e8b1811b7b70f1e9404074047f88b
Arun Kumar <arunkumar@Aruns-MacBook-Air.local>
2025-05-02 16:24:08 +0530

    demofinal

--- resume.py
+++ resume.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-from streamlit_extras.switch_page_button import switch_page
 from PIL import Image
 from pathlib import Path
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
